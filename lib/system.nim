841bef63fe9737712026314b1ab7beb37a97d0ee
Arne Döring <arne.doering@gmx.net>
2020-04-27 01:09:32 +0200

    do stuff

--- lib/system.nim
+++ lib/system.nim
@@ -2507,13 +2507,17 @@ template `..^`*(a, b: untyped): untyped =
   ## '..' and '^' is required.
   a .. ^b
 
-template `..<`*(a, b: untyped): untyped =
+template `..<`*[T](a: T; b: BackwardsIndex): untyped =
+  a .. succ(b)
+
+template `..<`*[T: not BackwardsIndex](a, b: T): untyped =
   ## A shortcut for `a .. pred(b)`.
   ##
   ## .. code-block:: Nim
   ##   for i in 5 ..< 9:
   ##     echo i # => 5; 6; 7; 8
-  a .. (when b is BackwardsIndex: succ(b) else: pred(b))
+  a .. pred(b)
+
 
 template spliceImpl(s, a, L, b: untyped): untyped =
   # make room for additional elements or cut:
