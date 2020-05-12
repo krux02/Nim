771758befa7a4a5d4babb24a45c2e193d8afeb8f
Arne Döring <arne.doering@gmx.net>
2020-05-12 23:13:18 +0200

    remove term rewriting macros tests

--- tests/template/template_various.nim
+++ tests/template/template_various.nim
@@ -103,27 +103,6 @@ type
 converter to_myfloat(x: float): MyFloat {.inline.} =
   MyFloat(val: x)
 
-block pattern_with_converter:
-  proc `+`(x1, x2: MyFloat): MyFloat =
-    MyFloat(val: x1.val + x2.val)
-
-  proc `*`(x1, x2: MyFloat): MyFloat =
-      MyFloat(val: x1.val * x2.val)
-
-  template optMul{`*`(a, 2.0)}(a: MyFloat): MyFloat =
-    a + a
-
-  func floatMyFloat(x: MyFloat): MyFloat =
-    result = x * 2.0
-
-  func floatDouble(x: float): float =
-    result = x * 2.0
-
-  doAssert floatDouble(5) == 10.0
-
-
-
-
 block procparshadow:
   template something(name: untyped) =
     proc name(x: int) =
